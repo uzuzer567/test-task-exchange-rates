d1cfda42b743ba814914188dacd04c8a54bbf3fd
Valeryia Stasko <valeryiastasko@Noutbuk-Valeryia.local>
2023-04-26 14:34:38 -0700

    feature: add no-value pipe and translate service

--- src/app/core/interfaces/rate.ts
+++ src/app/core/interfaces/rate.ts
@@ -3,6 +3,6 @@ import { RateCode } from '../../core/enums/rate-code';
 export interface Rate {
   code: RateCode;
   isSelected: boolean;
-  currentValue: number;
-  previousValue: number;
+  currentValue: number | null;
+  previousValue: number | null;
 }
---
+++ src/app/core/pipes/no-value.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'noValue',
+})
+export class NoValuePipe implements PipeTransform {
+  transform(value: any): number | string {
+    if (value === undefined || value === null || !Number.isFinite(value)) {
+      return '---';
+    } else {
+      return value.toFixed(6);
+    }
+  }
+}
